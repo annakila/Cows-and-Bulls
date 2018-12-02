268a0ad0fdf3bc016e3e16bbafd136afa76619cd
mconnelly2 <45365674+mconnelly2@users.noreply.github.com>
2018-12-02 15:08:30 -0600

    Create michaelbeep1.asm

---
+++ michaelbeep1.asm
@@ -0,0 +1,27 @@
+.text
+.globl main
+
+main:   
+
+	li $v0,31 
+	la $t0,beep 
+	lw $a0,0($t0) 
+	addi $t2,$a0,12 
+	la $t1,duration 
+	lbu $a1,0($t1) 
+	la $t3,volume 
+	lbu $a3,0($t3) 
+	move $t4,$a0 
+	move $t5,$a1 
+	move $t6,$a3 
+	syscall 
+
+
+syscall
+
+
+.data
+
+beep: .byte 72
+duration: .byte 100
+volume: .byte 127
